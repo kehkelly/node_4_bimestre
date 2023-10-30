7f535f69cc3f0c5d1a78b0608e2be46d22827c4c
EA - Kelly Dantas Da Silva Lima <dantas_kelly@educadventista.org>
2023-10-30 14:59:11 -0300

    Aula 2 concluída

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express()
 const path = require("path")
 const caminhoBase = path.join(__dirname, "templates")
 
+app.get('/usuarios/:id', (requisicao, resposta) => {
+    const id = requisicao.params.id
+
+    console.log(`Acessando dados do usuário ${id}`)
+    resposta.sendFile(`${caminhoBase}/usuarios.html`)
+})
+
 app.get('/', (requisicao, resposta) => {
     resposta.sendFile(`${caminhoBase}/index.html`)
 })
